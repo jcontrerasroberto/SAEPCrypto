07a3d763df4d259452a966742e78fe7282e4ccd3
jcontrerasroberto <contreras.jrobert@gmail.com>
2021-12-13 16:12:33 -0600

    Login working

---
+++ Server/src/main/java/DataBaseHandler.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+public class DataBaseHandler {
+
+    static Connection SAEPConn = null;
+    static PreparedStatement SAEPPrepareStat = null;
+
+    public DataBaseHandler(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            SAEPConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/saep", "root", "root");
+            if(SAEPConn!=null) log("Connected");
+            else log("Error connecting the BD");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User getUser(String id){
+        try {
+            SAEPPrepareStat = SAEPConn.prepareStatement("SELECT * FROM users WHERE user_id = ?");
+            SAEPPrepareStat.setString(1, id);
+            ResultSet rs = SAEPPrepareStat.executeQuery();
+            while (rs.next()){
+                User temp = new User();
+                temp.setId(rs.getString("user_id"));
+                temp.setName(rs.getString("user_name"));
+                temp.setRole(rs.getString("user_role"));
+                temp.setPassword(rs.getString("user_password"));
+                return temp;
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void log(String men) {
+        System.out.println(men);
+    }
+
+}
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -10,26 +10,40 @@ public class Server {
     private final Integer port = 9393;
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
+    private DataBaseHandler dbHandler;
 
     public Server(){
-        System.out.println("Starting server...");
+        log("Connecting to the DB");
+        dbHandler = new DataBaseHandler();
+        log("Starting server...");
         ServerSocket ss = null;
 
         try {
             ss = new ServerSocket(port);
             ss.setReuseAddress(true);
-            System.out.println("Waiting for clients");
+            log("Waiting for clients");
 
             while(true){
                 Socket socket = ss.accept();
                 oos = new ObjectOutputStream(socket.getOutputStream());
                 ois = new ObjectInputStream(socket.getInputStream());
-                System.out.println("Client connected");
+                log("Client connected");
+                login();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void login(){
+        User toValidate = (User) this.receiveObject();
+        User result = dbHandler.getUser(toValidate.getId());
+        if (result!=null && toValidate.getId().equals(result.getId()) && toValidate.getPassword().equals(result.getPassword())){
+            this.sendObject(result);
+        }else{
+            this.sendObject(null);
+        }
     }
 
     public void sendMessage(String mes){
@@ -71,6 +85,10 @@ public class Server {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private void log(String men) {
+        System.out.println(men);
     }
 
     public static void main(String[] args) {
