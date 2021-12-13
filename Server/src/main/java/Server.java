1e9d3ee8aa9c3d3de754fe040ca73b5e50530cad
stell <cristinatoes@gmail.com>
2021-12-12 23:36:48 -0600

    Holi

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,7 +1,7 @@
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Server iniciado");
+        System.out.println("Server iniciado Stella");
     }
 
 }
